43b74cf53aea042a27d3e52f64b511cc1103f5cd
sri vardhan <srivardhan3566@gmail.com>
2023-12-17 17:54:12 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -7,7 +7,15 @@ const nextConfig = {
               hostname: "oaidalleapiprodscus.blob.core.windows.net",
             }
         ]
-    }
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
+
 }
 
 module.exports = nextConfig
