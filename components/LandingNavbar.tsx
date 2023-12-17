57fc874eb508cf23283cd211004b7b53ffb353b1
sri vardhan <srivardhan3566@gmail.com>
2023-12-17 16:41:57 +0530

    commit

--- components/LandingNavbar.tsx
+++ components/LandingNavbar.tsx
@@ -7,19 +7,23 @@ const LandingNavbar = () => {
   const {isSignedIn} = useAuth();
   return (
     <>
-      <nav className="p-4 mx-3">
-        <div className="flex items-center justify-between">
-          <Link href="/" className="flex justify-center items-center gap-3">
+      <nav className="p-4 mx-3 relative">
+      
+        <div className="flex  w-full h-full">
+          <div className="mx-auto mt-52 flex flex-col space-y-6">
+          <Link href="/" className="flex justify-center items-center gap-3 -translate-x-5">
             <div className=" relative w-10 h-10">
               <Image fill src="/logo.png" alt="logo" />
             </div>
             <h1 className="text-white font-bold font-serif text-3xl">Genius</h1>
           </Link>
-            <Link href={isSignedIn ? "/dashboard" : "/sign-up"} className="rounded-lg">
-                <Button variant="outline" >
+            <Link href={isSignedIn ? "/dashboard" : "/sign-up"} className="rounded-lg ">
+                <Button  className="bg-gradient-to-r from-cyan-500 to-blue-500 hover:scale-105 transition-all duration-200">
                     Get Started
                 </Button>
             </Link>
+          </div>
+          
         </div>
         
       </nav>
